f535b814db08a7ad309d69d251fce150bc8e53ca
Pau Canosa <62012672+paucanosa@users.noreply.github.com>
2021-12-08 20:29:13 +0100

    Added new month histogram and more filters

--- scripts/basicVis.js
+++ scripts/basicVis.js
@@ -2,6 +2,8 @@ class BasicVis {
 
     constructor(params) {
         this.currentData = params.data;
+
+        // Severity bar chart data
         this.marginBarChart = { top: 30, right: 30, bottom: 70, left: 60 };
         this.widthBarChart = 587 - this.marginBarChart.left - this.marginBarChart.right;
         this.heightBarChart = 325 - this.marginBarChart.top - this.marginBarChart.bottom;
@@ -9,8 +11,33 @@ class BasicVis {
         this.yBarChart = null;
         this.xAxisBarChart = null;
         this.yAxisBarChart = null;
-        this.createBarChart();
 
+        // Histogram chart variables.
+        this.xHistogram = null;
+        this.yHistogram = null;
+        this.xAxisHistogram = null;
+        this.yAxisHistogram = null;
+        this.histogramFormatter = null;
+        this.marginHistogram = {
+            top: 10, right: 30, bottom: 30, left: 40
+        }
+        this.widthHistogram = 700 - this.marginHistogram.left - this.marginHistogram.right;
+        this.heightHistogram = 175 - this.marginHistogram.top - this.marginHistogram.bottom;
+
+        //Boxplot data
+        this.xBoxplot = null;
+        this.yBoxplot = null;
+        this.lineBoxplot = null;
+        this.rectBoxplot = null;
+        this.marginBoxplot = {
+            top: 10, right: 30, bottom: 30, left: 40
+        }
+        this.widthBoxplot = 700 - this.marginBoxplot.left - this.marginBoxplot.right;
+        this.heightBoxplot = 175 - this.marginBoxplot.top - this.marginBoxplot.bottom;
+
+        this.createBarChart();
+        this.createHistogramChart();
+        //this.createBoxPlotChart();
     }
 
     updateData(filteredData) {
@@ -92,9 +119,164 @@ class BasicVis {
     }
 
     updateCharts() {
-        const node = document.getElementById("basic-container");
         this.updateBarChart();
-        // node.innerHTML = '';
-        // node.innerHTML = "<p class='filters-title'>"+this.currentData.length+" car accidents</p>";
+        this.updateHistogramChart();
+        //this.createBoxPlotChart()
+    }
+
+
+    createHistogramChart() {
+        var svg = d3.select("#histogramchart").append("svg")
+            .attr("width", this.widthHistogram + this.marginHistogram.left + this.marginHistogram.right)
+            .attr("height", this.heightHistogram + this.marginHistogram.top + this.marginHistogram.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + this.marginHistogram.left + "," + this.marginHistogram.top + ")");
+
+        // set the ranges
+        this.xHistogram = d3.scaleTime()
+            .domain([new Date(2019, 0), new Date(2020, 0)])
+            .rangeRound([0, this.widthHistogram]);
+        this.yHistogram = d3.scaleLinear()
+            .range([this.heightHistogram, 0]);
+
+        this.yAxisHistogram = svg.append("g");
+        this.xAxisHistogram = svg.append("g").attr("transform", "translate(0," + this.heightHistogram + ")")
+        // set the parameters for the histogram
+        this.histogramFormatter = d3.bin()
+            .value(function (d) { return d; })
+            .domain(this.xHistogram.domain())
+            .thresholds(this.xHistogram.ticks(d3.timeMonth));
+        // append the svg object to the body of the page
+        // append a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+
+
+        // format the data
+        this.updateHistogramChart(svg)
+    }
+
+    updateHistogramChart(svgInput) {
+        //Data extraction and formatted
+        var chartData = this.currentData.map(function (d) { return d.Start_Time.slice(0, 7) });
+        var parseDate = d3.timeParse("%Y-%m");
+        chartData = chartData.map(function (d) {
+            return parseDate(d);
+        })
+
+        //Histogram bins creation
+        var bins = this.histogramFormatter(chartData)
+        this.yHistogram.domain([0, d3.max(bins, function (d) { return d.length; })]);
+
+        if (!svgInput) var svg = d3.select("#histogramchart").select("svg")
+        else var svg = svgInput
+
+        //Axis data updated
+        this.yAxisHistogram.transition().duration(1000).call(d3.axisLeft(this.yHistogram));
+        this.xAxisHistogram.call(d3.axisBottom(this.xHistogram));
+
+
+        const xHistogram = this.xHistogram;
+        const yHistogram = this.yHistogram;
+        const heightHistogram = this.heightHistogram
+
+        var u = svg.selectAll("rect").data(bins);
+        u.enter()
+            .append("rect")
+            .merge(u)
+            .transition()
+            .duration(1000)
+            .attr("class", "bar")
+            .attr("x", 1)
+            .attr("transform", function (d) {
+                return "translate(" + xHistogram(d.x0) + "," + yHistogram(d.length) + ")";
+            })
+            .attr("width", function (d) { const width = xHistogram(d.x1) - xHistogram(d.x0) - 1; if (width > 0) return width; else return 0; })
+            .attr("height", function (d) { return heightHistogram - yHistogram(d.length); })
+            .attr("fill", "#69b3a2");
+    }
+
+    createBoxPlotChart() {
+        // append the svg object to the body of the page
+        var node = document.getElementById('#boxplotchart');
+        if(node)node.innerHTML = '';
+
+        d3.select("#boxplotchart").select('svg')?.remove();
+
+        var svg = d3.select("#boxplotchart")
+            .append("svg")
+            .attr("width", this.widthBoxplot + this.marginBoxplot.left + this.marginBoxplot.right)
+            .attr("height", this.heightBoxplot + this.marginBoxplot.top + this.marginBoxplot.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(649 , -120) rotate(" + (90) + ")");
+
+        this.lineBoxplot = svg.append("line");
+        this.rectBoxplot = svg.append("rect")
+        this.updateBoxplotChart(svg);
+
+    }
+
+    updateBoxplotChart(svgInput) {
+       
+        // create dummy data
+        var chartData = this.currentData.map(function (d) { return d.Start_Time.slice(0, 7) });
+        var parseDate = d3.timeParse("%Y-%m");
+        chartData = chartData.map(function (d) {
+            return parseDate(d).getTime();
+        })
+
+
+        // Compute summary statistics used for the box:
+        var data_sorted = chartData.sort(d3.ascending)
+        var q1 = d3.quantile(data_sorted, .25)
+        var median = d3.quantile(data_sorted, .5)
+        var q3 = d3.quantile(data_sorted, .75)
+        var interQuantileRange = q3 - q1
+        var min = q1 - 1.5 * interQuantileRange
+        var max = q1 + 1.5 * interQuantileRange
+
+        var y = d3.scaleLinear()
+            .domain([d3.min(chartData), d3.max(chartData)])
+            .range([this.widthBoxplot - 172, 0]);
+
+        // svg.call(d3.axisRight(y))
+
+        // a few features for the box
+        var center = 200
+        var width = 100
+
+        if (!svgInput) var svg = d3.select("#boxplotchart").select("svg")
+        else var svg = svgInput
+        // Show the main vertical line
+        this.lineBoxplot
+            .attr("x1", center)
+            .attr("x2", center)
+            .attr("y1", y(min))
+            .attr("y2", y(max))
+            .attr("stroke", "black")
+
+        // Show the box
+        this.rectBoxplot
+            .attr("x", center - width / 2)
+            .attr("y", y(q3))
+            .attr("height", (y(q1) - y(q3)))
+            .attr("width", width)
+            .attr("stroke", "black")
+            .style("fill", "#69b3a2")
+
+        // show median, min and max horizontal lines
+        var u = svg.selectAll("toto").data([min, median, max]);
+        u
+            .enter()
+            .append("line")
+            .merge(u)
+            .transition()
+            .duration(1000)
+            .attr("x1", center - width / 2)
+            .attr("x2", center + width / 2)
+            .attr("y1", function (d) { return (y(d)) })
+            .attr("y2", function (d) { return (y(d)) })
+            .attr("stroke", "black")
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,279 @@
-var promises = [d3.csv("data/main2.csv")];
+//FIXME: Don't know why it does not let me import the state data.
+// import {statesData} from "./logicData";
+
+var statesData = [
+  { 'label': 'Alabama', 'value': 'AL' },
+  { 'label': 'Alaska', 'value': 'AK' },
+  { 'label': 'American Samoa', 'value': 'AS' },
+  { 'label': 'Arizona', 'value': 'AZ' },
+  { 'label': 'Arkansas', 'value': 'AR' },
+  { 'label': 'California', 'value': 'CA' },
+  { 'label': 'Colorado', 'value': 'CO' },
+  { 'label': 'Connecticut', 'value': 'CT' },
+  { 'label': 'Delaware', 'value': 'DE' },
+  { 'label': 'District of Columbia', 'value': 'DC' },
+  { 'label': 'States of Micronesia', 'value': 'FM' },
+  { 'label': 'Florida', 'value': 'FL' },
+  { 'label': 'Georgia', 'value': 'GA' },
+  { 'label': 'Guam', 'value': 'GU' },
+  { 'label': 'Hawaii', 'value': 'HI' },
+  { 'label': 'Idaho', 'value': 'ID' },
+  { 'label': 'Illinois', 'value': 'IL' },
+  { 'label': 'Indiana', 'value': 'IN' },
+  { 'label': 'Iowa', 'value': 'IA' },
+  { 'label': 'Kansas', 'value': 'KS' },
+  { 'label': 'Kentucky', 'value': 'KY' },
+  { 'label': 'Louisiana', 'value': 'LA' },
+  { 'label': 'Maine', 'value': 'ME' },
+  { 'label': 'Marshall Islands', 'value': 'MH' },
+  { 'label': 'Maryland', 'value': 'MD' },
+  { 'label': 'Massachusetts', 'value': 'MA' },
+  { 'label': 'Michigan', 'value': 'MI' },
+  { 'label': 'Minnesota', 'value': 'MN' },
+  { 'label': 'Mississippi', 'value': 'MS' },
+  { 'label': 'Missouri', 'value': 'MO' },
+  { 'label': 'Montana', 'value': 'MT' },
+  { 'label': 'Nebraska', 'value': 'NE' },
+  { 'label': 'Nevada', 'value': 'NV' },
+  { 'label': 'New Hampshire', 'value': 'NH' },
+  { 'label': 'New Jersey', 'value': 'NJ' },
+  { 'label': 'New Mexico', 'value': 'NM' },
+  { 'label': 'New York', 'value': 'NY' },
+  { 'label': 'North Carolina', 'value': 'NC' },
+  { 'label': 'North Dakota', 'value': 'ND' },
+  { 'label': 'Northern Mariana Islands', 'value': 'MP' },
+  { 'label': 'Ohio', 'value': 'OH' },
+  { 'label': 'Oklahoma', 'value': 'OK' },
+  { 'label': 'Oregan', 'value': 'OR' },
+  { 'label': 'Palau', 'value': 'PW' },
+  { 'label': 'Pennsilvania', 'value': 'PA' },
+  { 'label': 'Puerto Rico', 'value': 'PR' },
+  { 'label': 'Rhode Island', 'value': 'RI' },
+  { 'label': 'South Carolina', 'value': 'SC' },
+  { 'label': 'South Dakota', 'value': 'SD' },
+  { 'label': 'Tennessee', 'value': 'TN' },
+  { 'label': 'Texas', 'value': 'TX' },
+  { 'label': 'Utah', 'value': 'UT' },
+  { 'label': 'Vermont', 'value': 'VT' },
+  { 'label': 'Virgin Islands', 'value': 'VI' },
+  { 'label': 'Virginia', 'value': 'VA' },
+  { 'label': 'Washington', 'value': 'WA' },
+  { 'label': 'West Virginia', 'value': 'WV' },
+  { 'label': 'Wisconsin', 'value': 'WI' },
+  { 'label': 'Wyoming', 'value': 'WY' }
+];
+var weatherConditions = ['Light Rain', 'Overcast', 'Mostly Cloudy', 'Snow', 'Light Snow', 'Cloudy', '', 'Scattered Clouds', 'Clear', 'Partly Cloudy', 'Light Freezing Drizzle', 'Light Drizzle', 'Haze', 'Rain', 'Heavy Rain', 'Fair', 'Drizzle', 'Fog', 'Thunderstorms and Rain', 'Patches of Fog', 'Light Thunderstorms and Rain', 'Mist', 'Rain Showers', 'Light Rain Showers', 'Heavy Drizzle', 'Smoke', 'Light Freezing Fog', 'Light Freezing Rain', 'Blowing Snow', 'Heavy Thunderstorms and Rain', 'Heavy Snow', 'Snow Grains', 'Squalls', 'Light Fog', 'Shallow Fog', 'Thunderstorm', 'Light Ice Pellets', 'Thunder', 'Thunder in the Vicinity', 'Fair / Windy', 'Light Rain with Thunder', 'Heavy Thunderstorms and Snow', 'Light Snow Showers', 'Cloudy / Windy', 'Ice Pellets', 'N/A Precipitation', 'Light Thunderstorms and Snow', 'T-Storm', 'Rain / Windy', 'Wintry Mix', 'Partly Cloudy / Windy', 'Heavy T-Storm', 'Sand', 'Light Rain / Windy', 'Widespread Dust', 'Mostly Cloudy / Windy', 'Blowing Dust / Windy', 'Blowing Dust', 'Volcanic Ash', 'Freezing Rain / Windy', 'Small Hail', 'Wintry Mix / Windy', 'Light Snow / Windy', 'Heavy Ice Pellets', 'Heavy Snow / Windy', 'Heavy Rain / Windy', 'Heavy T-Storm / Windy', 'Fog / Windy', 'Dust Whirls', 'Showers in the Vicinity', 'Funnel Cloud', 'Haze / Windy', 'Light Rain Shower', 'Smoke / Windy', 'Light Drizzle / Windy', 'Snow / Windy', 'Partial Fog', 'Light Freezing Rain / Windy', 'Sleet', 'Blowing Snow / Windy', 'Snow and Sleet', 'Snow and Sleet / Windy', 'Squalls / Windy', 'Light Sleet / Windy', 'Freezing Drizzle', 'Freezing Rain', 'Thunder / Windy', 'Drizzle and Fog', 'Light Sleet', 'Thunder / Wintry Mix / Windy', 'Mist / Windy', 'Light Snow and Sleet', 'Rain Shower', 'Sleet / Windy', 'T-Storm / Windy', 'Light Snow and Sleet / Windy', 'Patches of Fog / Windy', 'Drizzle / Windy', 'Sand / Dust Whirls Nearby', 'Light Rain Shower / Windy', 'Heavy Rain Shower', 'Thunder and Hail', 'Drifting Snow', 'Light Snow Shower', 'Sand / Dust Whirlwinds', 'Heavy Blowing Snow', 'Hail', 'Heavy Freezing Drizzle', 'Low Drifting Snow', 'Light Blowing Snow', 'Heavy Rain Showers', 'Light Haze', 'Heavy Thunderstorms with Small Hail', 'Light Snow with Thunder', 'Heavy Snow with Thunder', 'Thunder and Hail / Windy', 'Tornado']
+var promises = [d3.csv("data/main3.csv")];
+var numericalConditionsOrder = ['=', '&gt;', '≥', '&lt;', '≤', 'x'];
+var numericalConditionsMap = {
+  temperature: 'Temperature(F)',
+  humidity: 'Humidity(%)',
+  visibility: 'Visibility(mi)',
+  windspeed: 'Wind_Speed(mph)',
+  precipitation: 'Precipitation(in)'
+}
 var currentData = data;
 var data;
 var activeFilters = [];
 var basicVis;
 var geoVis;
 
-Promise.all(promises).then(function(files){
+Promise.all(promises).then(function (files) {
   data = files[0]
   currentData = data;
   init();
 })
 
-function init(){
+function init() {
   activeFilters = {
-    severity : [],
-    states:[],
-    causes: []
+    severity: [],
+    states: [],
+    causes: [],
+    weatherConditions: [],
+    humidity: { value: null, condition: 'x' },
+    temperature: { value: null, condition: 'x' },
+    visibility: { value: null, condition: 'x' },
+    windspeed: { value: null, condition: 'x' },
+    precipitation: { value: null, condition: 'x' }
   }
   const params = {
-    data : currentData
+    data: currentData
   }
+  initFiltersElements()
   basicVis = new BasicVis(params);
   geoVis = new GeoVis(params);
 }
 
-function initFiltersElements(){
-  // TODO: CREATE THIS METHOD TO INITIALIZE THE FILTERS SECTION
+function initFiltersElements() {
+  var parentNode = document.getElementById('statesfilters');
+  for (let i = 0; i < statesData.length; ++i) {
+    var div = document.createElement("div")
+    div.className = "filters-conditional-item filters-conditional-item--states"
+    var input = document.createElement("input")
+    input.type = "checkbox"
+    input.id = "f_" + statesData[i].value;
+    input.value = "f_" + statesData[i].value;
+    input.onclick = function () { changeFilter('States', statesData[i].value) };
+    var label = document.createElement("label")
+    label.htmlFor = "f_" + statesData[i].value;
+    label.className = "filter-label"
+    label.innerHTML = statesData[i].label;
+    div.appendChild(input)
+    div.appendChild(label)
+    parentNode.appendChild(div);
+  }
+
+  parentNode = document.getElementById('weatherconditions');
+  for (let i = 0; i < weatherConditions.length; ++i) {
+    var div = document.createElement("div")
+    div.className = "filters-conditional-item filters-conditional-item--states"
+    var input = document.createElement("input")
+    input.type = "checkbox"
+    input.id = "f_" + weatherConditions[i];
+    input.value = "f_" + weatherConditions[i];
+    input.onclick = function () { changeFilter('Weather Conditions', weatherConditions[i]) };
+    var label = document.createElement("label")
+    label.htmlFor = "f_" + weatherConditions[i];
+    label.className = "filter-label"
+    label.innerHTML = weatherConditions[i];
+    div.appendChild(input)
+    div.appendChild(label)
+    parentNode.appendChild(div);
+  }
 }
 
-function changeFilter(type,value){
-  if(type=="Severity"){
-    if(activeFilters.severity.includes(value)) activeFilters.severity = activeFilters.severity.filter(function(row){return row != value});
+function setNumericalFiltersMinAndMax() {
+  const visibilityValues = data.map(function (row) { return row['Visibility(mi)'] });
+  document.getElementById('visibilitySlider').min = Math.min(visibilityValues)
+  document.getElementById('visibilitySlider').max = Math.max(visibilityValues)
+
+  const windspeedValues = data.map(function (row) { return row['Wind_Speed(mph)'] });
+  document.getElementById('windspeedSlider').min = Math.min(windspeedValues)
+  document.getElementById('windspeedSlider').max = Math.max(windspeedValues)
+
+  const precipitationValues = data.map(function (row) { return row['Precipitation(in)'] });
+  document.getElementById('visibilitySlider').min = Math.min(precipitationValues)
+  document.getElementById('visibilitySlider').max = Math.max(precipitationValues)
+}
+
+function changeFilter(type, value) {
+  if (type == "Severity") {
+    if (activeFilters.severity.includes(value)) activeFilters.severity = activeFilters.severity.filter(function (row) { return row != value });
     else activeFilters.severity.push(value)
   }
-  if(type=="States"){
-    if(activeFilters.states.includes(value)) activeFilters.states = activeFilters.states.filter(function(row){return row != value});
+  else if (type == "States") {
+    if (activeFilters.states.includes(value)) activeFilters.states = activeFilters.states.filter(function (row) { return row != value });
     else activeFilters.states.push(value)
+
   }
-  if(type=="Cause"){
-    if(activeFilters.causes.includes(value)) activeFilters.causes = activeFilters.causes.filter(function(row){return row != value});
+  else if (type == "Cause") {
+    if (activeFilters.causes.includes(value)) activeFilters.causes = activeFilters.causes.filter(function (row) { return row != value });
     else activeFilters.causes.push(value)
   }
+  else if (type == "Weather Conditions") {
+    if (activeFilters.weatherConditions.includes(value)) activeFilters.weatherConditions = activeFilters.weatherConditions.filter(function (row) { return row != value });
+    else activeFilters.weatherConditions.push(value)
+  }
+  else {
+    activeFilters[type].value = value;
+    const nodeSlider = document.getElementById(type + 'Slide');
+    const nodeInput = document.getElementById(type + 'Input');
+    if (!isNaN(Number(value))) {
+      if (nodeSlider.max >= Number(value) && nodeSlider.min <= Number(value)) {
+        nodeInput.value = value;
+        nodeSlider.value = value;
+      } else {
+        nodeInput.value = nodeSlider.min;
+        nodeSlider.value = nodeSlider.min;
+      }
+    }
+  }
+  console.log(activeFilters)
 }
 
- 
-function applyFilters(){
-  var newData = data;
-  if(activeFilters.severity.length > 0)
-    newData = newData.filter(function(row){ return activeFilters.severity.includes(row['Severity']) });
+function changeNumericalCondition(filtertype) {
+  var node = document.getElementById(filtertype + 'condition');
+  const currentCondition = node.innerHTML;
+  const index = numericalConditionsOrder.indexOf(currentCondition);
+  if (index == numericalConditionsOrder.length - 1) {
+    node.innerHTML = numericalConditionsOrder[0].toString()
+    node.className = "numerical-condition-button"
+    activeFilters[filtertype].condition = numericalConditionsOrder[0]
+  } else {
+    node.innerHTML = numericalConditionsOrder[index + 1]
+    node.className = index == numericalConditionsOrder.length - 2 ? "numerical-condition-button numerical-condition-button--off" : "numerical-condition-button"
+    activeFilters[filtertype].condition = numericalConditionsOrder[index + 1]
+    console.log(index)
+    console.log(numericalConditionsOrder[index + 1])
+  }
+  console.log(activeFilters)
+}
 
-  if(activeFilters.states.length > 0)
-    newData = newData.filter(function(row){ return activeFilters.states.includes(row['State']) });
-    
-  for(let i = 0; i < activeFilters.causes.length; ++i){
-    newData = newData.filter(function(row){ return row[activeFilters.causes[i]]=="True"});
+
+function applyFilters() {
+  var newData = data;
+  if (activeFilters.severity.length > 0)
+    newData = newData.filter(function (row) { return activeFilters.severity.includes(row['Severity']) });
+
+  if (activeFilters.states.length > 0)
+    newData = newData.filter(function (row) { return activeFilters.states.includes(row['State']) });
+
+  for (let i = 0; i < activeFilters.causes.length; ++i) {
+    newData = newData.filter(function (row) { return row[activeFilters.causes[i]] == "True" });
   }
 
+  for (let i = 0; i < activeFilters.weatherConditions.length; ++i) {
+    newData = newData.filter(function (row) { return activeFilters.weatherConditions.includes(row['Weather_Condition']) });
+  }
+
+  const numericalConditions = ['temperature', 'visibility', 'humidity', 'windspeed', 'precipitation']
+  for (let i = 0; i < numericalConditions.length; ++i) {
+    if (activeFilters[numericalConditions[i]].condition != 'x') {
+      console.log(activeFilters[numericalConditions[i]].condition)
+      
+      switch (activeFilters[numericalConditions[i]].condition) {
+        case '=':
+          newData = newData.filter(function (row) {
+            return row[numericalConditionsMap[numericalConditions[i]]] == Number(activeFilters[numericalConditions[i]].value)
+          });
+          break;
+        case '&gt;':
+          // console.log('Greater than')
+          // console.log(Number(activeFilters[numericalConditions[i]].value))
+          // console.log(numericalConditionsMap[numericalConditions[i]])
+          console.log(newData)
+          newData = newData.filter(function (row) {
+            // console.log(parseInt(row[numericalConditionsMap[numericalConditions[i]]]),Number(activeFilters[numericalConditions[i]].value))
+            // console.log(parseInt(row[numericalConditionsMap[numericalConditions[i]]]) > parseInt(activeFilters[numericalConditions[i]].value))
+            return parseInt(row[numericalConditionsMap[numericalConditions[i]]]) > Number(activeFilters[numericalConditions[i]].value)
+          });
+          console.log(newData)
+          break;
+        case '≥':
+          newData = newData.filter(function (row) {
+            return row[numericalConditionsMap[numericalConditions[i]]] >= Number(activeFilters[numericalConditions[i]].value)
+          });
+          break;
+        case '&lt;':
+          newData = newData.filter(function (row) {
+            return row[numericalConditionsMap[numericalConditions[i]]] < Number(activeFilters[numericalConditions[i]].value)
+          });
+          break;
+        case '≤':
+          newData = newData.filter(function (row) {
+            return row[numericalConditionsMap[numericalConditions[i]]] <= Number(activeFilters[numericalConditions[i]].value)
+          });
+          break;
+
+      }
+      console.log(numericalConditionsMap[numericalConditions[i]])
+    }
+  }
   currentData = newData;
   updateData()
 }
 
-function updateData(){
+function updateData() {
   geoVis.updateData(currentData);
   basicVis.updateData(currentData);
 }
