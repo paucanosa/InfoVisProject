c67cfc3aaab956c38b3c5e7aca39f6e1b4df3863
Marciano <marci_jorden@msn.com>
2021-12-10 19:40:18 +0100

    Implement working heatmap

--- scripts/geoVis.js
+++ scripts/geoVis.js
@@ -3,17 +3,20 @@ class GeoVis {
     constructor(params){
         this.currentData = params.data;
         this.statesData = params.statesData;
-        this.points = [];
+        this.stateMapping = {};
+        this.stateCounts = {};
+        this.stateCountStats = {};
+
+        this.statesData.forEach(state => this.stateMapping[state.value] = state.label)
+
         this.projection = null;
         this.pointPixelSize = 3.0;
 
-        this.createGeographicalChart()
-        this.updateCharts();
+        this.createGeographicalChart();
     }
 
     updateData(filteredData){
         this.currentData = filteredData;
-        console.log("GeoVis recieved new data!")
         this.updateCharts();
     }
 
@@ -34,29 +37,16 @@ class GeoVis {
         var path = d3.geoPath();
         var g = svg.append("g");
 
-        // Load states
-        d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json").then( us => {
-            g.attr("class", "states")
-                .selectAll("path")
-                .data(topojson.feature(us, us.objects.states).features)
-                .enter().append("path")
-                .attr("fill", "lightblue")
-                .attr("d", path)
-                .style("stroke", "white");
-        });
-
-        var zoom = d3.zoom()
-            .scaleExtent([1, 100])
-            .on('zoom', event => {
-                g.selectAll('path')
-                    .attr('transform', event.transform);
-                
-                svg.selectAll('circle')
-                    .attr('transform', event.transform)
-                    .attr("r", "" + (this.pointPixelSize / (event.transform.k ** 0.8)) + "px");
-            });
-
-        svg.call(zoom);
+        // var geoVis = this;
+        // var samplePoints = [[-80, 30]];
+        // add circles to svg
+        // svg.selectAll("circle")
+        //     .data(samplePoints).enter()
+        //     .append("circle")
+        //     .attr("cx", d => this.projection(d)[0])
+        //     .attr("cy", d => this.projection(d)[1])
+        //     .attr("r", "" + this.pointPixelSize + "px")
+        //     .attr("fill", "black");
 
         this.updateGeographicalChart();
     }
@@ -72,19 +62,75 @@ class GeoVis {
 
         this.points = this.currentData.map(d => [parseFloat(d['Start_Lng']), parseFloat(d['Start_Lat'])]);
 
-        // Remove points from svg that aren't present
-        svg.selectAll("circle")
-            .data(this.points)
+        this.statesData.forEach(stateData => this.stateCounts[stateData.label] = 0);
+        this.currentData.forEach(row => this.stateCounts[this.stateMapping[row.State]] += 1);
+        
+        this.stateCountStats = {
+            'min': 0,
+            'max': 0,
+            'total': 0,
+            'mean': 0.0
+        };
+
+        for (let [state, count] of Object.entries(this.stateCounts)) {
+            this.stateCountStats.total += count;
+            if (count < this.stateCountStats.min) {
+                this.stateCountStats.min = count;
+            }
+            if (count > this.stateCountStats.max) {
+                this.stateCountStats.max = count;
+            }
+        };
+
+        this.stateCountStats.mean = parseFloat(this.stateCountStats.total) / Object.entries(this.stateCounts).length
+
+        var colorScale = d3.scaleLinear()
+            .domain([-0.000001,this.stateCountStats.max])
+            .range(["lightblue", "darkblue"]);
+
+        var stateCounts = this.stateCounts;
+
+        svg.selectAll("path")
+            .data([])
             .exit()
             .remove();
 
-        // add circles to svg
-        svg.selectAll("circle")
-            .data(this.points).enter()
-            .append("circle")
-            .attr("cx", d => this.projection(d)[0])
-            .attr("cy", d => this.projection(d)[1])
-            .attr("r", "" + this.pointPixelSize + "px")
-            .attr("fill", "red");
+        d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json").then( us => {
+            g.attr("class", "states")
+                .selectAll("path")
+                .data(topojson.feature(us, us.objects.states).features)
+                .enter().append("path")
+                .attr("fill", d => colorScale(stateCounts[d.properties.name]-0.000001))
+                .attr("d", path)
+                .style("stroke", "white");
+        });
+
+        // // Remove points from svg that aren't present
+        // svg.selectAll("circle")
+        //     .data(this.points)
+        //     .exit()
+        //     .remove();
+
+        // // add circles to svg
+        // svg.selectAll("circle")
+        //     .data(this.points).enter()
+        //     .append("circle")
+        //     .attr("cx", d => this.projection(d)[0])
+        //     .attr("cy", d => this.projection(d)[1])
+        //     .attr("r", "" + this.pointPixelSize + "px")
+        //     .attr("fill", "red");
+
+        var zoom = d3.zoom()
+            .scaleExtent([1, 100])
+            .on('zoom', event => {
+                g.selectAll('path')
+                    .attr('transform', event.transform);
+                
+                svg.selectAll('circle')
+                    .attr('transform', event.transform)
+                    .attr("r", "" + (this.pointPixelSize / (event.transform.k ** 0.8)) + "px");
+            });
+
+        svg.call(zoom);
     }
 }
